2d00b85d773150b50a7bd5afbfe9df54e4e9f3ff
FerrixFox <englishle86@gmail.com>
2023-07-21 07:49:47 -0400

    Update age.js with a for loop

---
+++ loops/age.js
@@ -0,0 +1,9 @@
+let rlSync = require("readline-sync");
+
+let age = Number(rlSync.question("How old are you?\n"));
+
+console.log(`You are ${age} years old.`);
+
+for(let i = 1; i < 5; i++){
+  console.log(`In ${i * 10} years, you will be ${age + (i*10)} years old.`);
+}
