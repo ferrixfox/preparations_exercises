12ff8469a790bbcfc50759ee011d898d3b44d158
FerrixFox <englishle86@gmail.com>
2023-07-21 07:59:09 -0400

    make a factorial function to hold the loop

--- loops/factorial.js
+++ loops/factorial.js
@@ -1,9 +1,11 @@
-let rlSync = require("readline-sync");
-
-let num = Number(rlSync.question("Enter a number: "));
-
-for(let i = (num - 1); i > 0; i--){
-  num = num * i;
+function factorial(num){
+  for(let i = (num - 1); i > 0; i--){
+    num = num * i;
+  }
+  console.log(num)
 }
 
-console.log(num);
+let rlSync = require("readline-sync");
+let num = Number(rlSync.question("Enter a number: "));
+
+factorial(num);
